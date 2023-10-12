34cfb67486a34b0c0036b0cdc300ed5c745a3231
PgmJun <chltmdwns96@gmail.com>
2023-10-13 02:06:54 +0900

    main 클래스 SpringBootApplication 으로 변환

---
+++ seminar1/src/main/java/org/example/ServerSeminar1Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSeminar1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSeminar1Application.class, args);
+	}
+}
