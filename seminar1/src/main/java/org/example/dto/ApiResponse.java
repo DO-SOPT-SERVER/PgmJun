e5bb27efafac0354c5836e27c57a4e255e1ea575
PgmJun <chltmdwns96@gmail.com>
2023-10-13 02:26:24 +0900

    refactor: ApiResponse DTO 생성자 캡슐화 #1

--- seminar1/src/main/java/org/example/dto/ApiResponse.java
+++ seminar1/src/main/java/org/example/dto/ApiResponse.java
@@ -2,11 +2,12 @@ package org.example.dto;
 
 import org.springframework.http.HttpStatus;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse {
 
 	private final int code;
